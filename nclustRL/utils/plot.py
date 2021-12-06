b827b4cf5dd755d64ed6b02a6dcbc98b49708646
pedro <pgcotovio@gmail.com>
2021-12-06 14:53:02 +0000

    [init] data gather functions

--- nclustRL/utils/plot.py
+++ nclustRL/utils/plot.py
@@ -8,29 +8,22 @@ import os.path as osp
 import numpy as np
 
 
-def plot_data(data, xaxis='Epoch', value="AverageEpRet", condition="Condition1", smooth=1, **kwargs):
-    if smooth > 1:
-        """
-        smooth data with moving window average.
-        that is,
-            smoothed_y[t] = average(y[t-k], y[t-k+1], ..., y[t+k-1], y[t+k])
-        where the "smooth" param is width of that window (2k+1)
-        """
-        y = np.ones(smooth)
-        for datum in data:
-            x = np.asarray(datum[value])
-            z = np.ones(len(x))
-            smoothed_x = np.convolve(x,y,'same') / np.convolve(z,y,'same')
-            datum[value] = smoothed_x
+def get_tunning_results(df):
 
-    if isinstance(data, list):
-        data = pd.concat(data, ignore_index=True)
+    pass
+
+
+def get_training_results(*args):
+
+    pass
+
+
+def plot_data(data, xaxis='Epoch', value="AverageEpRet", condition="Condition1", **kwargs):
+
     sns.set(style="darkgrid", font_scale=1.5)
     sns.lineplot(data=data, x=xaxis, y=value, hue=condition, ci='sd', **kwargs)
 
     plt.legend(loc='best').set_draggable(True)
-    #plt.legend(loc='upper center', ncol=3, handlelength=1,
-    #           borderaxespad=0., prop={'size': 13})
 
     xscale = np.max(np.asarray(data[xaxis])) > 5e3
     if xscale:
