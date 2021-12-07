ee229f91c80c4f97a8a8ddd0541db9c91ca5abcb
pedro <pgcotovio@gmail.com>
2021-12-07 13:44:28 +0000

    [update] env_registry

--- nclustRL/utils/registry.py
+++ nclustRL/utils/registry.py
@@ -1,10 +1,12 @@
 from ray.tune.registry import register_env as register
 from ray.rllib.models import ModelCatalog
+from gym.wrappers import TransformObservation
+from nclustRL.utils.helper import transform_obs
 import nclustenv
 
 
 def register_env(id):
-    return register(id, lambda config: nclustenv.make(id, **config))
+    return register(id, lambda config: TransformObservation(nclustenv.make(id, **config), transform_obs))
 
 
 def register_model(id, model):
