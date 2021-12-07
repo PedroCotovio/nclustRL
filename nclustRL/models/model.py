a88fcbc7b2272f97e93a81ebce35eff292f66eb5
pedro <pgcotovio@gmail.com>
2021-12-07 13:48:45 +0000

    [init] models

--- nclustRL/models/model.py
+++ nclustRL/models/model.py
@@ -1,3 +1,45 @@
+import dgl
+import dgl.nn.pytorch as dglnn
+import torch.nn as nn
+from torch.nn import functional as F
 
-class RlModel:
-    pass
+
+class RGCN(nn.Module):
+    def __init__(self, in_feats, hid_feats, out_feats, rel_names):
+        super().__init__()
+
+        self.conv1 = dglnn.HeteroGraphConv({
+            rel: dglnn.GraphConv(in_feats, hid_feats)
+            for rel in rel_names}, aggregate='sum')
+        self.conv2 = dglnn.HeteroGraphConv({
+            rel: dglnn.GraphConv(hid_feats, out_feats)
+            for rel in rel_names}, aggregate='sum')
+
+    def forward(self, graph, inputs):
+        # inputs is features of nodes
+        h = self.conv1(graph, inputs)
+        h = {k: F.relu(v) for k, v in h.items()}
+        h = self.conv2(graph, h)
+        return h
+
+
+class HeteroClassifier(nn.Module):
+    def __init__(self, in_dim, hidden_dim, n_classes, rel_names):
+        super().__init__()
+
+        self.rgcn = RGCN(in_dim, hidden_dim, hidden_dim, rel_names)
+        self.classify = nn.Linear(hidden_dim, n_classes)
+
+    def forward(self, g):
+        h = g.ndata['feat']
+        h = self.rgcn(g, h)
+        with g.local_scope():
+            g.ndata['h'] = h
+            # Calculate graph representation by average readout.
+            hg = 0
+            for ntype in g.ntypes:
+                hg = hg + dgl.mean_nodes(g, 'h', ntype=ntype)
+            return self.classify(hg)
+
+    class RlModel:
+        pass
