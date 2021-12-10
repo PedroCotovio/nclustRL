8cd243112e1725984431ed31c781015dab539cf5
Pedro Cotovio <pgcotovio@gmail.com>
2021-12-10 00:33:01 +0000

    [implement][models] graph encoder

--- nclustRL/models/model.py
+++ nclustRL/models/model.py
@@ -2,43 +2,101 @@ import dgl
 import dgl.nn.pytorch as dglnn
 import torch.nn as nn
 from torch.nn import functional as F
+from nclustRL.utils.helper import pairwise
+
+class HeteroRelu(nn.ReLU):
+
+    def __init__(self, inplace:bool = False):
+        super(HeteroRelu, self).__init__(inplace=inplace)
+
+    def forward(self, inputs):
+        
+        return {k: super(HeteroRelu, self).forward(v) for k, v in inputs.items()}
+
+class GraphSequential(nn.Sequential):
+
+    def __init__(self, *args):
+        super(GraphSequential, self).__init__(*args)
+
+    def forward(self, graph, feat, edge_weight=None):
+        for module in self:
+
+            if isinstance(module, dglnn.HeteroGraphConv):
+
+                rel_names = zip(module.mods.keys(), graph.canonical_etypes)
+                feat = module(
+                    g=graph, 
+                    inputs=feat, 
+                    mod_kwargs={
+                        rel: dict(edge_weight=graph.edges[canonical].data[edge_weight]) 
+                        for rel, canonical in rel_names})
+
+            else:
+                feat = module(inputs=feat)
+
+        return feat
 
 
 class RGCN(nn.Module):
-    def __init__(self, in_feats, hid_feats, out_feats, rel_names):
+    def __init__(self, layers, rel_names):
         super().__init__()
 
-        self.conv1 = dglnn.HeteroGraphConv({
-            rel: dglnn.GraphConv(in_feats, hid_feats)
-            for rel in rel_names}, aggregate='sum')
-        self.conv2 = dglnn.HeteroGraphConv({
-            rel: dglnn.GraphConv(hid_feats, out_feats)
-            for rel in rel_names}, aggregate='sum')
+        _layers = []
 
-    def forward(self, graph, inputs):
-        # inputs is features of nodes
-        h = self.conv1(graph, inputs)
-        h = {k: F.relu(v) for k, v in h.items()}
-        h = self.conv2(graph, h)
-        return h
+        for in_feats, out_feats in pairwise(layers): 
+
+            _layers.append(dglnn.HeteroGraphConv({
+                rel: dglnn.GraphConv(in_feats, out_feats)
+                for rel in rel_names}, aggregate='sum'))
+
+            _layers.append(HeteroRelu())
+
+        self._hidden_layers = GraphSequential(*_layers)
+
+            
+
+    def forward(self, graph, feat, edge_weight=None):
+
+        return self._hidden_layers(graph, feat, edge_weight)
 
 
-class HeteroClassifier(nn.Module):
-    def __init__(self, in_dim, hidden_dim, n_classes, rel_names):
+class GraphEncoder(nn.Module):
+    def __init__(self, n, conv_feats, n_classes, rel_names):
         super().__init__()
 
-        self.rgcn = RGCN(in_dim, hidden_dim, hidden_dim, rel_names)
-        self.classify = nn.Linear(hidden_dim, n_classes)
+        conv_feats.insert(0, n)
+        self.rgcn = RGCN(conv_feats, rel_names)
 
     def forward(self, g):
         h = g.ndata['feat']
-        h = self.rgcn(g, h)
+        h = self.rgcn(g, h, 'w')
         with g.local_scope():
             g.ndata['h'] = h
-            # Calculate graph representation by average readout.
             hg = 0
-            for ntype in g.ntypes:
+            for ntype in h.keys():
                 hg = hg + dgl.mean_nodes(g, 'h', ntype=ntype)
+            
+            return hg
+
+
+class HeteroClassifier(nn.Module):
+    def __init__(self, n, conv_feats, n_classes, rel_names):
+        super().__init__()
+
+        conv_feats.insert(0, n)
+
+        self.rgcn = RGCN(conv_feats, rel_names)
+        self.classify = nn.Linear(conv_feats[-1], n_classes)
+
+    def forward(self, g):
+        h = g.ndata['feat']
+        h = self.rgcn(g, h, 'w')
+        with g.local_scope():
+            g.ndata['h'] = h
+            hg = 0
+            for ntype in h.keys():
+                hg = hg + dgl.mean_nodes(g, 'h', ntype=ntype)
+
             return self.classify(hg)
 
 class RlModel:
