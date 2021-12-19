69938c2aa8407a7a009bebef6a2dadaf0d597456
Pedro Cotovio <pgcotovio@gmail.com>
2021-12-19 19:35:02 +0000

    [modify] import models

--- nclustRL/models/__init__.py
+++ nclustRL/models/__init__.py
@@ -1,10 +1,7 @@
-from nclustenv.version import ENV_LIST as _ENV_LIST
 from nclustRL.utils import registry
 
-from .model import RlModel
+from .pre_train import RGCN, HeteroClassifier
+from .model import GraphEncoder, RlModel
 
-# register envs in ray
-for env in _ENV_LIST:
-    registry.register_env(env)
 
 registry.register_model('RlModel', RlModel)
