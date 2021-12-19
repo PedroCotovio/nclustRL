754173e2b7b390a9164eb5a8bd2737f6f4bf25ab
Pedro Cotovio <pgcotovio@gmail.com>
2021-12-19 19:35:37 +0000

    [modify][main init] init ray and register envs

--- nclustRL/__init__.py
+++ nclustRL/__init__.py
@@ -2,13 +2,27 @@
 __author__      = "Pedro Cotovio"
 __license__     = 'GNU GPLv3'
 
-from nclustenv.version import VERSION as __version__
-
 import os
 import sys
 import warnings
+import ray
 
+# init ray
+try:
+    ray.init()
+except RuntimeError:
+    pass
+
+
+from .version import VERSION as __version__
 from .trainer import Trainer
 from . import utils
 from . import models
 from . import configs
+
+# register envs in ray
+from .utils import registry
+from nclustenv.version import ENV_LIST as _ENV_LIST
+
+for env in _ENV_LIST:
+    registry.register_env(env)
